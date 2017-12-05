c27b976f6258d0553c72b10a14e9f6c7ed1d352a
Steve <doublefly@live.cn>
2017-12-05 13:38:23 +0800

    init

---
+++ DanMu/Controllers/DanMuController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DanMu.Controllers
+{
+    [Route("api/[controller]")]
+    public class DanMuController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<DanmuData> Get()
+        {
+            return SocketHandler.historicalMessg;
+        }
+
+        // GET api/values/5
+        [HttpGet("online")]
+        public int GetOnline()
+        {
+            return SocketHandler.Sockets.Count;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DanMu/DanmuData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DanMu
+{
+    public class DanmuData
+    {
+        public string Text { get; set; }
+    }
+}
